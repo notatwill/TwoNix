c5c4943b3b6d6189b2657f66287b2105da8cd2e1
notatwill <30223844+notatwill@users.noreply.github.com>
2025-10-26 18:40:00 -0500

    update flake refs

--- flake.nix
+++ flake.nix
@@ -1,11 +1,20 @@
 {
-  description = "Simple flake with a devshell";
+  description = "v2 NixOS config";
 
   # Add all your dependencies here
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable-small";
     blueprint.url = "github:numtide/blueprint";
     blueprint.inputs.nixpkgs.follows = "nixpkgs";
+    disko.url = "github:nix-community/disko";
+    disko.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    sops-nix.url = "github:Mic92/sops-nix";
+    sops-nix.inputs.nixpkgs.follows = "nixpkgs";
+    stylix.url = "github:danth/stylix";
+    stylix.inputs.nixpkgs.follows = "nixpkgs";
+    nixos-facter-modules.url = "github:nix-community/nixos-facter-modules";
   };
 
   # Load the blueprint
