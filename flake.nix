c9d25e5cae268a184e3491a8ad831dd74c04fe38
notatwill <30223844+notatwill@users.noreply.github.com>
2025-10-26 20:07:26 -0500

    setup devtools

--- flake.nix
+++ flake.nix
@@ -6,17 +6,26 @@
     nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable-small";
     blueprint.url = "github:numtide/blueprint";
     blueprint.inputs.nixpkgs.follows = "nixpkgs";
+    blueprint.inputs.systems.follows = "systems";
     disko.url = "github:nix-community/disko";
     disko.inputs.nixpkgs.follows = "nixpkgs";
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    nixos-facter-modules.url = "github:nix-community/nixos-facter-modules";
     sops-nix.url = "github:Mic92/sops-nix";
     sops-nix.inputs.nixpkgs.follows = "nixpkgs";
     stylix.url = "github:danth/stylix";
     stylix.inputs.nixpkgs.follows = "nixpkgs";
-    nixos-facter-modules.url = "github:nix-community/nixos-facter-modules";
+    stylix.inputs.systems.follows = "systems";
+    systems.url = "github:nix-systems/default";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+    treefmt-nix.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   # Load the blueprint
-  outputs = inputs: inputs.blueprint { inherit inputs; };
+  outputs = inputs:
+    inputs.blueprint {
+      inherit inputs;
+      prefix = "nix/";
+    };
 }
---
+++ nix/formatter.nix
@@ -0,0 +1,47 @@
+{
+  pkgs,
+  inputs,
+  ...
+}:
+inputs.treefmt-nix.lib.mkWrapper pkgs {
+  projectRootFile = "flake.nix";
+
+  programs = {
+    alejandra.enable = true;
+    deadnix.enable = true;
+    gofumpt.enable = true;
+    prettier.enable = true;
+    statix.enable = true;
+  };
+
+  settings = {
+    global.excludes = [
+      "LICENSE"
+      "test/*"
+      # unsupported extensions
+      # "*.{gif,png,svg,tape,mts,lock,mod,sum,toml,env,envrc,gitignore,pages}"
+    ];
+
+    formatter = {
+      deadnix = {
+        priority = 1;
+      };
+
+      statix = {
+        priority = 2;
+      };
+
+      alejandra = {
+        priority = 3;
+      };
+
+      prettier = {
+        options = [
+          "--tab-width"
+          "4"
+        ];
+        includes = ["*.{css,html,js,json,jsx,md,mdx,scss,ts,yaml}"];
+      };
+    };
+  };
+}
