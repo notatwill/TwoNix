ef559288076367ee975e3a5af9ff28a780365c8d
notatwill <30223844+notatwill@users.noreply.github.com>
2025-11-23 15:17:45 -0600

    add betelgeuse

---
+++ nix/hosts/betelgeuse/configuration.nix
@@ -0,0 +1,13 @@
+{flake, ...}: {
+  imports = [
+    flake.nixosModules.workstation
+    ./disko.nix
+    ./hardware.nix
+    ./users.nix
+  ];
+  time.timeZone = "America/Chicago";
+  networking.hostName = "betelgeuse";
+  networking.hostId = "1352f34a"; # random, required by zfs
+  system.stateVersion = "25.05";
+  vars.persistence.enable = true;
+}
