dec661b1c1b3c22f81f10b5fe5bdeef50a7611ab
notatwill <30223844+notatwill@users.noreply.github.com>
2025-11-23 20:10:32 -0600

    remove git module, remove jj signing

--- nix/modules/home/earth/jujutsu.nix
+++ nix/modules/home/earth/jujutsu.nix
@@ -1,13 +1,4 @@
-{config, ...}: {
-  programs.jujutsu = {
-    enable = true;
-    settings = {
-      signing = {
-        behavior = "drop";
-        backend = "ssh";
-        key = config.sops.secrets."ssh_signing_key".path;
-      };
-      git.sign-on-push = false; # change to true later https://github.com/jj-vcs/jj/issues/7853
-    };
-  };
+{pkgs,...}: {
+  programs.jujutsu.enable = true;
+  home.packages = [ pkgs.git ];
 }
