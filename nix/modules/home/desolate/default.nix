90336b49b15ffacbe08dc334ea28f1593d496613
hybrideology <30223844+hybrideology@users.noreply.github.com>
2026-01-06 23:19:43 -0600

    cleanup home for desolate

--- nix/modules/home/desolate/default.nix
+++ nix/modules/home/desolate/default.nix
@@ -2,11 +2,8 @@ _: {
   imports = [
     ../required
     ../btop.nix
-    ../git.nix
     ../helix.nix
     ../nh.nix
-    ../nixos-anywhere.nix
-    ../sops.nix
     ../ssh.nix
     ../unar.nix
     ../yazi.nix
--- nix/modules/home/nh.nix
+++ nix/modules/home/nh.nix
@@ -1,4 +1,5 @@
-_: {
+{pkgs, ...}: {
+  home.packages = [pkgs.git]; # needed for referencing git flakes
   programs.nh = {
     enable = true;
     clean = {
