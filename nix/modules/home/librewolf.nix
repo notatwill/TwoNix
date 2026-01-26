761ad340fdcf1a4438933404f182fd215346797a
hybrideology <30223844+hybrideology@users.noreply.github.com>
2026-01-25 20:22:27 -0600

    configure librewolf further

--- nix/modules/home/librewolf.nix
+++ nix/modules/home/librewolf.nix
@@ -7,7 +7,49 @@ _: {
       DisableAppUpdate = true;
       DisableSetDesktopBackround = true;
       DisableSystemAddonUpdate = true;
+      ExtensionSettings = {
+        "{446900e4-71c2-419f-a6a7-df9c091e268b}" = {
+          default_area = "navbar";
+          install_url = "https://addons.mozilla.org/firefox/downloads/latest/bitwarden-password-manager/latest.xpi";
+          installation_mode = "force_installed";
+        };
+      };
+    };
+    profiles.default = {
+      isDefault = true;
+      bookmarks = {
+        force = true;
+        settings = [
+          {
+            name = "Nix sites";
+            toolbar = true;
+            bookmarks = [
+              {
+                name = "homepage";
+                url = "https://nixos.org/";
+              }
+              {
+                name = "wiki";
+                tags = ["wiki" "nix"];
+                url = "https://wiki.nixos.org/";
+              }
+              {
+                name = "search";
+                url = "https://search.nixos.org/packages";
+              }
+              {
+                name = "home-manager search";
+                url = "https://home-manager-options.extranix.com/";
+              }
+            ];
+          }
+        ];
+      };
+      settings = {
+        "browser.startup.page" = 3;
+      };
     };
   };
-  vars.persistence.dirs = [".config/librewolf"];
+  stylix.targets.librewolf.profileNames = ["default"];
+  vars.persistence.dirs = [".librewolf"];
 }
