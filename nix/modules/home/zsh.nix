3b814bae992ce73770b02abc478cf27c9cd86f30
hybrideology <30223844+hybrideology@users.noreply.github.com>
2026-01-26 12:01:50 -0600

    add ssh terminal fix

--- nix/modules/home/zsh.nix
+++ nix/modules/home/zsh.nix
@@ -6,6 +6,9 @@ _: {
       enable = true;
       plugins = ["colorize" "colored-man-pages" "ssh" "sudo"];
     };
+    initContent = ''
+      [[ "$TERM" == "xterm-kitty" ]] && alias ssh="TERM=xterm-256color ssh"
+    '';
   };
   programs.kitty.settings.shell = "zsh";
   vars.persistence.files = [".zsh_history"];
