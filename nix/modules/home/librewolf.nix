d6810eb5e3b035f1790ab11630e7453bab70e4d2
hybrideology <30223844+hybrideology@users.noreply.github.com>
2026-01-25 20:22:27 -0600

    add librewolf policies

--- nix/modules/home/librewolf.nix
+++ nix/modules/home/librewolf.nix
@@ -1,35 +1,13 @@
 _: {
   programs.librewolf = {
     enable = true;
-    # policies = {
-    #   BlockAboutConfig = true;
-    #   BlockAboutAddons = true;
-    #   BlockAboutProfiles = true;
-    #   AutofillAddressEnabled = false;
-    #   AutofillCreditCardEnabled = false;
-    #   CaptivePortal = true; # allows detection of captive portals
-    #   DisableAppUpdate = true;
-    #   DisableFirefoxAccounts = true;
-    #   DisableFirefoxStudies = true;
-    #   DisablePocket = true;
-    #   DisableSetDesktopBackround = true;
-    #   DisableSystemAddonUpdate = true;
-    #   DisableTelemetry = true;
-    #   DontCheckDefaultBrowser = true;
-    #   EnableTrackingProtection = true;
-    #   FirefoxHome = {
-    #     SponsoredTopSites = false;
-    #     Highlights = false;
-    #     SponsoredPocket = false;
-    #     SponsoredStories = false;
-    #   };
-    #   FirefoxSuggest = {
-    #     SponsoredSuggestions = false;
-    #     ImproveSuggest = false;
-    #   };
-    #   HardwareAcceleration = true;
-    #   GenerativeAI.Enabled = false;
-    # };
+    policies = {
+      BlockAboutConfig = true;
+      BlockAboutProfiles = true;
+      DisableAppUpdate = true;
+      DisableSetDesktopBackround = true;
+      DisableSystemAddonUpdate = true;
+    };
   };
   vars.persistence.dirs = [".config/librewolf"];
 }
