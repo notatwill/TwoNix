f37ae09a275dfdac401c0ccaf427ed0cfcae3d2e
hybrideology <30223844+hybrideology@users.noreply.github.com>
2026-02-06 00:19:56 -0600

    add secret path

--- nix/hosts/ceres/secrets.nix
+++ nix/hosts/ceres/secrets.nix
@@ -7,6 +7,7 @@
     sopsFile = inputs.secrets.ceres-ddns-updater;
     owner = "ddns-updater";
     group = "ddns-updater";
+    path = "/etc/ddns-updater/config.json";
     format = "binary";
   };
   services.ddns-updater.environment.CONFIG_FILEPATH = config.sops.secrets.ceres-ddns-updater.path;
