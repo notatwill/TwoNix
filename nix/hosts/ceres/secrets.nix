5914c2dded94af263f7fdb5693b63c93c587f11d
hybrideology <30223844+hybrideology@users.noreply.github.com>
2026-02-06 00:11:02 -0600

    fix secret permissions

--- nix/hosts/ceres/secrets.nix
+++ nix/hosts/ceres/secrets.nix
@@ -5,6 +5,8 @@
 }: {
   sops.secrets.ceres-ddns-updater = {
     sopsFile = inputs.secrets.ceres-ddns-updater;
+    owner = "ddns-updater";
+    group = "ddns-updater";
     format = "binary";
   };
   services.ddns-updater.environment.CONFIG_FILEPATH = config.sops.secrets.ceres-ddns-updater.path;
