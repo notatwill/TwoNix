bae79fbec30e7f71f3098ea2618201190109880c
hybrideology <30223844+hybrideology@users.noreply.github.com>
2026-02-06 21:48:00 -0600

    fix string

--- nix/hosts/ceres/secrets.nix
+++ nix/hosts/ceres/secrets.nix
@@ -6,7 +6,7 @@
   sops.secrets.ceres-ddns-updater = {
     sopsFile = inputs.secrets.ceres-ddns-updater;
     path = "/etc/ddns-updater/config.json";
-    mode = 444;
+    mode = "444";
     format = "binary";
   };
   services.ddns-updater.environment.CONFIG_FILEPATH = config.sops.secrets.ceres-ddns-updater.path;
