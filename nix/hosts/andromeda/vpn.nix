0aa9c906c35facb11b2190810ef7bcd305148864
hybrideology <30223844+hybrideology@users.noreply.github.com>
2026-02-08 22:32:42 -0600

    attempt to fix dns config

--- nix/hosts/andromeda/vpn.nix
+++ nix/hosts/andromeda/vpn.nix
@@ -17,7 +17,6 @@
         matchConfig.Name = "wg0";
         address = ["10.0.0.2/24"];
         dns = ["10.0.0.1"];
-        domains = ["ceres"];
       };
     };
     netdevs = {
--- nix/hosts/ceres/dns-server.nix
+++ nix/hosts/ceres/dns-server.nix
@@ -1,5 +1,11 @@
 {config, ...}: {
-  services.dnsmasq.enable = true; # serves from /etc/hosts
-  services.resolved.enable = false; # enabled by systemd networking
+  networking.firewall = {
+    allowedUDPPorts = [53];
+    allowedTCPPorts = [53];
+  };
+  services = {
+    dnsmasq.enable = true; # serves from /etc/hosts
+    resolved.enable = false; # enabled by systemd networking
+  };
   environment.persistence.${config.vars.persistence.dir}.directories = ["/var/lib/dnsmasq"];
 }
