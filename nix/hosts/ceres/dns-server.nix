6125f5213ae19aba4d993b417edd4d483e12b9e8
hybrideology <30223844+hybrideology@users.noreply.github.com>
2026-02-09 01:14:55 -0600

    switch back to resolved

--- nix/hosts/ceres/dns-server.nix
+++ nix/hosts/ceres/dns-server.nix
@@ -3,14 +3,9 @@ _: {
     allowedUDPPorts = [53];
     allowedTCPPorts = [53];
   };
-  services.dnsmasq = {
-    enable = true;
-    settings = {
-      interface = "wg1";
-      bind-interfaces = true;
-      listen-address = "10.0.0.1";
-    };
-  };
   # Keep resolved for local resolution, but disable its DNS server
-  services.resolved.settings.Resolve.DNSStubListener = "no";
+  services.resolved.settings.Resolve = {
+    DNSStubListener = "yes";
+    DNSStubListenerExtra = "10.0.0.1";
+  };
 }
